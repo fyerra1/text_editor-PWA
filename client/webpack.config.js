6ea6208af59d966c5732b1855b186162e6880792
Franky Erra <frankyerra43@gmail.com>
2022-08-24 21:24:26 -0700

    modified manifest identity

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'Just Another Text Editor'
       }),
      
       // Injects our custom service worker
@@ -60,9 +60,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Take notes with javascript syntax highlighting',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
